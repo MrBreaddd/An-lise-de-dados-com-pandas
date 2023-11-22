2d7fd6f4611fbc9e31b5972ac459206077e855ca
MrBreaddd <nathanmorais5@hotmail.com>
2023-11-22 09:58:12 -0300

    Cálculo de editora que mais vendeu

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -5,9 +5,12 @@ import pandas as pd;
 livros_df = pd.read_excel('Vendas rede de livrarias(os 100 titulos mais vendidos).xlsx');
 #Abre a tabela e a atribui à variável livros_df
 
+
+
 media_vendastotais = livros_df['Cópias vendidas'].mean();
 print("Média total de vendas da livraria:", media_vendastotais, "cópias.");
 #Destaca a coluna de copias vendidas, calcula a média dos valores e atribua à variável media_vendas, então imprime o valor
+
 
 
 terror = livros_df['Gênero'] == 'Terror';
@@ -45,8 +48,24 @@ print("O gênero menos vendido é Comédia com", generos_vendidos.min().min(), "
 #Filtragem para o maior e menor valor e imprime.
 
 
+
 livros_mais_vendidos = livros_df[['Título', 'Número de páginas', 'Cópias vendidas']].sort_values(by='Cópias vendidas', ascending=False).head(10);
 #Organiza os top 10 livros levando em consideração o número de cópias vendidas
 
-print("A média de páginas dos top 10 livros mais vendidos é de", livros_mais_vendidos['Número de páginas'].mean(), "páginas.");
+livros_mais_vendidos_media = livros_mais_vendidos['Número de páginas'].mean();
+#Calcula a média de páginas e atribui à variável não estilizada
+
+print("A média de páginas dos top 10 livros mais vendidos é de",livros_mais_vendidos_media , "páginas.");
 #Imprime a média do número de páginas dos top 10
+
+
+
+editora_vendas = livros_df[['Editora', 'Cópias vendidas']].groupby('Editora')['Cópias vendidas'].sum().reset_index();
+#Organiza as vendas por nome de editora, forma alternativa e mais eficiente em comparação
+# com o que foi feito com generos (linha 16 a 48).
+
+editora_mais_vendas = editora_vendas.sort_values(by='Cópias vendidas', ascending=False).head(1).reset_index();
+#Separa a editora com mais vendas.
+
+print("A editora que mais vendeu é Intrínseca com", editora_mais_vendas['Cópias vendidas'].to_string(index=False), "cópias vendidas.");
+#Imprime, forma alternativa de imprimir a partir de um dataframe
