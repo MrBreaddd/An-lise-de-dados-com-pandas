b6b2e3c5b0b8fe9c18e83b7bf257a3c67454dd8a
MrBreaddd <nathanmorais5@hotmail.com>
2023-11-22 07:39:33 -0300

    Adição de calculo de média de vendas

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -2,5 +2,11 @@ from IPython.display import display
 import pandas as pd;
 
 
-livros_df = pd.read_excel('Pasta1.xlsx');
-display(livros_df);
+livros_df = pd.read_excel('Vendas rede de livrarias(os 100 titulos mais vendidos).xlsx');
+#Abre a tabela e a atribui à variável livros_df
+
+media_vendastotais = livros_df['Cópias vendidas'].mean();
+#Destaca a coluna de copias vendidas, calcula a média dos valores e atribua à variável media_vendas
+
+
+#display(livros_df['Gênero'].columns());
